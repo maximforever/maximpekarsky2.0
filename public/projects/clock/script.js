fe794c3401228903817e37cfd04913c19c2cd416
pekarsky.maxim@gmail.com <flufci@gmail.com>
2018-12-13 11:46:48 -0500

    fix screen height for firefox

--- public/projects/clock/script.js
+++ public/projects/clock/script.js
@@ -1,7 +1,9 @@
+var triggerEvent = (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) ? "touchstart" : "click";
+
 init();
 
 var TEST_TIME;
-test = false;
+var test = false;
 
 var previousTime = {};
 var animation = null;
@@ -36,10 +38,13 @@ function getFormattedTime(){
 
     var now = new Date();
 
+    now.setHours(12);    
+    now.setMinutes(14);
+
     var hours = now.getHours();
     var minutes = now.getMinutes();
 
-    return formatTime(hours, minutes)
+    return formatTime(hours, minutes);
 }
 
 // format digits to class names
@@ -51,14 +56,14 @@ function formatTime(hours, minutes){
         teenMinutes: null,
         tensOfMinutes: null,
         singleMinutes: null
-    }
+    };
 
     if(hours > 12){
         hours -=12;
-        formattedTime.am = false
+        formattedTime.am = false;
     }
 
-    if (hours == 0){ hours = 12 }
+    if (hours == 0){ hours = 12; }
 
     formattedTime.hours = hours + "-hours";
 
@@ -83,14 +88,7 @@ function formatTime(hours, minutes){
 // physically update clock on screen
 function refreshClock(time){
 
-    var selectedWords = document.getElementsByClassName("word");
-
-    //remove all selected elements
-    for(var i = 0; i < selectedWords.length; i++){
-        selectedWords[i].className = "word";
-    }   
-
-    console.log(time.hours);
+    clearStyling();
 
     document.getElementById(time.hours).className += " selected";
 
@@ -119,19 +117,31 @@ function enableButtons(){
     var buttons = document.getElementsByClassName("change-color");
 
     for(var i = 0; i < buttons.length; i++){
-        buttons[i].addEventListener("click", function(e){
-            console.log(e.target.id);
+        buttons[i].addEventListener(triggerEvent, function(e){
             document.getElementById("app").className = e.target.id;
 
             if(e.target.id == "christmas"){
                 startChristmasAnimation();
             } else {
                 clearInterval(animation);
-                animation = null;
+
+                // remove any christmas colors from selected words
+                previousTime = null;
+                updateTime();   
             }
-        })
+        });
     }
 
+}
+
+// clear any selected words 
+
+function clearStyling(){
+    var selectedWords = document.getElementsByClassName("word");
+    //remove all selected elements
+    for(var i = 0; i < selectedWords.length; i++){
+        selectedWords[i].className = "word";
+    }   
 }
 
 // randomly pick a light with a random color every half a second
@@ -139,7 +149,6 @@ function startChristmasAnimation(){
 
     animation = setInterval(function(){
 
-        var defaultColor = "#f9ff40";
         var selectedLights = document.getElementsByClassName("selected");
         var randomColorClass = "word selected random-color-" + Math.floor(Math.random()*4 + 1);          // these are four colors
 
@@ -147,13 +156,11 @@ function startChristmasAnimation(){
             currentLightCounter = 0;
         }
 
-
         for(var i = 0; i < selectedLights.length; i++){
             selectedLights[i].className =  (i == currentLightCounter) ? randomColorClass : "word selected";
         }
 
         currentLightCounter++;
-
 
     }, 500);
 }
